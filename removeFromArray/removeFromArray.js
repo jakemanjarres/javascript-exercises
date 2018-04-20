3dadbde431205b7fbf1b87fd6382b43f96cba6cf
U-TMS\manjarj <TMS+manjarj@PL9733903.tfs.toyota.com>
2018-04-20 16:18:46 -0500

    Created a removeFromArray function that passes given test cases

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-var removeFromArray = function() {
-
+var removeFromArray = function(array) {
+  //Create an array of the arguments passed to the function
+  var args = [...arguments];
+    //If no removal arguments are passed to the function, return message saying so
+    if(args.length === 1){
+      return "No removal arguments passed to function.";
+    } else{
+      //Remove the array object from the array of arguments
+      args.shift();
+      //Loop through the passed array, and remove items specified by removal arguments
+      for(var i = 0; i < args.length; i++){
+        for(var y = 0; y < array.length; y++){
+          if(array[y] === args[i]){
+            array.splice(y , 1);
+          }
+        }
+      }
+      return array;
+    }
 }
 
 module.exports = removeFromArray
