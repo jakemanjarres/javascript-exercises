f355e05495f84444618738e0297e7c26964ab79c
U-TMS\manjarj <TMS+manjarj@PL9733903.tfs.toyota.com>
2018-04-20 14:43:08 -0500

    Wrote function that passes all test cases

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-var repeatString = function() {
+var repeatString = function(string, repetitions) {
+  var stringProduct = '';
+  if(repetitions < 0){
+    return "ERROR";
+  }
+  if(repetitions === 0){
+    return stringProduct;
+  }
 
+  for(var i = 0; i < repetitions; i++){
+  stringProduct += string;
+ }
+ return stringProduct;
 }
 
 module.exports = repeatString
