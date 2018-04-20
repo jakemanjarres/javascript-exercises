0b44ea29f5c34e4186cc5190d3b0d5c690ecdfdb
U-TMS\manjarj <TMS+manjarj@PL9733903.tfs.toyota.com>
2018-04-20 15:05:52 -0500

    Wrote a reverseString function that passes all test cases

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,15 @@
-var reverseString = function() {
+var reverseString = function(stringToReverse) {
+  if(stringToReverse === ''){
+    return '';
+  }
+  var stringArray = stringToReverse.split("")
+  var reversedArray = [];
+
+  for(var i = 0; i < stringToReverse.length; i++){
+    reversedArray[i] = stringArray.pop();
+  }
+
+  return reversedArray.join('');
 
 }
 
