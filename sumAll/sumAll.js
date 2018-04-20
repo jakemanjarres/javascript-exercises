f5fd247921da44b457f372dff921dd4a8e0c8579
U-TMS\manjarj <TMS+manjarj@PL9733903.tfs.toyota.com>
2018-04-20 16:49:04 -0500

    Wrote a sumAll function that passes provided test cases

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,34 @@
-var sumAll = function() {
+var sumAll = function(firstInt, secondInt) {
+  var sum = 0;
 
+  //Check that both passed arguments are of type 'number'
+  if(typeof firstInt === 'number' && typeof secondInt === 'number'){
+    //If both numbers are the same, just add them together
+    if(firstInt === secondInt) {
+      return firstInt + secondInt;
+    }
+    //Check for negative int arguments
+    if(firstInt < 0 || secondInt < 0){
+      return "ERROR";
+    }
+    //If the first int is bigger, count up from second int and sum
+    if(firstInt > secondInt){
+      for(var i = secondInt; i <= firstInt; i++){
+        sum += i;
+      }
+      return sum;
+    }
+    //If the second int is bigger, count up from first int and sum
+    if(secondInt > firstInt){
+      for(var i = firstInt; i <= secondInt; i++){
+        sum += i;
+      }
+      return sum;
+    }
+  } else{
+    //Returns error if one of the arguments is not a true number.
+    return "ERROR"
+  }
 }
 
 module.exports = sumAll
