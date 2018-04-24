01fc7e4ce0fa04509b5772e846ad969db6f737c7
U-TMS\MANJARJ <TMS+MANJARJ@PL9733903.tfs.toyota.com>
2018-04-24 11:16:15 -0500

    Wrote conversion functions that pass given test cases

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-var ftoc = function() {
-  
+var ftoc = function(temperature) {
+  var conversion = (temperature - 32) * 5/9;
+  return Math.round(conversion*10)/10;
 }
 
-var ctof = function() {
-  
+var ctof = function(temperature) {
+  var conversion = temperature * 9/5 + 32;
+  return Math.round(conversion*10)/10;
 }
 
 module.exports = {
