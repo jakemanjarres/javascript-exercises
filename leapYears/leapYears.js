bcced6a2bbda4ab0dc7e5b9579ee953e57d54f69
U-TMS\MANJARJ <TMS+MANJARJ@PL9733903.tfs.toyota.com>
2018-04-24 10:46:53 -0500

    Wrote leapyear function that passes test cases

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,19 @@
-var leapYears = function() {
+var leapYears = function(year) {
+  var isLeapYear = false;
+  divisibleBy4 = year % 4;
+  divisibleBy100 = year % 100;
+  divisibleBy400 = year % 400;
 
+  if(divisibleBy4 == 0){
+    isLeapYear = true;
+    if(divisibleBy100 == 0){
+      isLeapYear = false;
+      if(divisibleBy400 == 0){
+        isLeapYear = true;
+      }
+    }
+  }
+  return isLeapYear;
 }
 
 module.exports = leapYears
